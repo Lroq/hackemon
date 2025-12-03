8470a597fa289ad8cbb3d4a116ca5bbfe9f68676
hdylan <hebrard.dylan@ynov.com>
2025-12-03 14:11:54 +0100

    ADD readfile v2 + db server V1

--- server/readfile.js
+++ server/readfile.js
@@ -2,23 +2,31 @@ const fs = require('fs');
 const path = require('path');
 
 function readEnvFile() {
-    const envFilePath = __dirname + "/.env"
+    const envFilePath = path.join(process.cwd(), ".env");
 
     try {
         const fileContent = fs.readFileSync(envFilePath, 'utf8');
         const envVariables = {};
 
         fileContent.split('\n').forEach(line => {
-            const [key, value] = line.split('=');
-            if (key && value) {
-                envVariables[key.trim()] = value.trim();
-            }
+            line = line.trim();
+
+            if (!line || line.startsWith('#')) return;
+
+            const index = line.indexOf('=');
+            if (index === -1) return;
+
+            const key = line.slice(0, index).trim();
+            const value = line.slice(index + 1).trim();
+
+            envVariables[key] = value;
         });
 
         return envVariables;
+
     } catch (error) {
         console.error(`Error reading the .env file: ${error.message}`);
-        return null;
+        return {};
     }
 }
 
